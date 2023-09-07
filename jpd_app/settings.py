0d06f73cbd57b5c3ea6c6d5a071e5db555776847
TheJonry <110304328+TheJonry@users.noreply.github.com>
2023-09-07 03:46:13 -0400

    fix 11

--- jpd_app/settings.py
+++ jpd_app/settings.py
@@ -122,7 +122,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-# AUTH_USER_MODEL = 'jpd_app.UserProfile'
+AUTH_USER_MODEL = 'jpd_app.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
