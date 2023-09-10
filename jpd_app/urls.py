63df4971e59eda7a73af1b891637c40db0143d40
TheJonry <110304328+TheJonry@users.noreply.github.com>
2023-09-09 23:02:46 -0400

    1

--- jpd_app/urls.py
+++ jpd_app/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.urls import path, include
-# from . import views
-# from django.conf import settings
-# from django.conf.urls.static import static
-# from django.contrib import admin
-# from jpd_app.views import *
+from django.urls import path, include
+from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.contrib import admin
+from jpd_app.views import *
 
 
 # urlpatterns = [
@@ -35,8 +35,8 @@ Including another URLconf
   
 # ]
 
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-# urlpatterns += [
-#      path('accounts/', include('django.contrib.auth.urls')),
-# ]
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += [
+     path('accounts/', include('django.contrib.auth.urls')),
+]
